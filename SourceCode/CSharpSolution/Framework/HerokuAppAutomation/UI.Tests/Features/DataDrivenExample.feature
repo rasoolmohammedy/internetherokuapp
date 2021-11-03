5036987f12eca45bda8b5d212d33fc0c1fa31e78
Rasool Mohammed <rasoolmohammedy@gmail.com>
2021-11-03 12:52:37 +0530

    Data Driven Test Case added to the UI test suite

---
+++ SourceCode/CSharpSolution/Framework/HerokuAppAutomation/UI.Tests/Features/DataDrivenExample.feature
@@ -0,0 +1,17 @@
+﻿Feature: DataDrivenExample
+	Simple Data Driven Example Test case
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+	Examples:
+	| param1 | param2 | result |
+	| 10     | 20     | 30     |
+	| 15     | 10     | 25     |
+	| 350    | 50     | 400    |
+	| 456    | 70     | 526    |
+	| 20     | 40     | 60     |
+	| 30     | 80     | 110    |
---
+++ SourceCode/CSharpSolution/Framework/HerokuAppAutomation/UI.Tests/StepDefinitions/DataDrivenExampleSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Utilities;
+
+namespace UI.Tests.StepDefinitions
+{
+    [Binding]
+    public class DataDrivenExampleSteps
+    {
+        int parama;
+        int paramb;
+        int result;
+        int expected;
+
+        [Given]
+        public void GivenTheFirstNumberIs_P0(int p0)
+        {
+            parama = p0;
+        }
+        
+        [Given]
+        public void GivenTheSecondNumberIs_P0(int p0)
+        {
+            paramb = p0;
+        }
+        
+        [When]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            result = parama + paramb;
+        }
+        
+        [Then]
+        public void ThenTheResultShouldBe_P0(int p0)
+        {
+            expected = p0;
+            if (result == expected)
+                ExtentReportsHelper.SetStepStatusPass($"Adding parama {parama} with paramb {paramb} is equal to {result} which is equal to expected output {expected}");
+            else
+                ExtentReportsHelper.SetTestStatusFail($"Adding parama {parama} with paramb {paramb} is equal to {result} which is not equal to expected output {expected}");
+        }
+    }
+}
